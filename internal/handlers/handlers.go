6054d5ac8f21265bf4e123ae1e3220eb60f6bfa2
REUP GANG <55562336+heavensfavorite@users.noreply.github.com>
2025-07-28 20:52:35 -0700

    login & logout

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -377,6 +377,45 @@ func (m *Repository) ShowLogin(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+// PostShowLogin handles logging the user in
 func (m *Repository) PostShowLogin(w http.ResponseWriter, r *http.Request) {
-	log.Println("works")
+	_ = m.App.Session.RenewToken(r.Context())
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	form := forms.New(r.PostForm)
+	form.Required("email", "password")
+	form.IsEmail("email")
+	if !form.Valid() {
+		render.Template(w, r, "login.page.tmpl", &models.TemplateData{
+			Form: form,
+		})
+		return
+	}
+
+	id, _, err := m.DB.Authenticate(email, password)
+	if err != nil {
+		log.Println(err)
+
+		m.App.Session.Put(r.Context(), "error", "Invalid login credentials")
+		http.Redirect(w, r, "user/login", http.StatusSeeOther)
+		return
+	}
+
+	m.App.Session.Put(r.Context(), "user_id", id)
+	m.App.Session.Put(r.Context(), "error", "Logged in successfully")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+// Logout logs a user out
+func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.Destroy(r.Context())
+	_ = m.App.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -4,12 +4,18 @@ import (
 	"database/sql"
 
 	"github.com/heavensfavorite/bookings/internal/config"
+	"github.com/heavensfavorite/bookings/internal/models"
 	"github.com/heavensfavorite/bookings/internal/repository"
 )
 
 type postgresDBRepo struct {
 	App *config.AppConfig
 	DB  *sql.DB
+}
+
+// GetUserByID implements repository.DatabaseRepo.
+func (m *postgresDBRepo) GetUserByID(id int) (models.User, error) {
+	panic("unimplemented")
 }
 
 type testDBRepo struct {
