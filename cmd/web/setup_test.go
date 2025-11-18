38aedc6b75278f00c91d907182472618102e96b5
REUP GANG <55562336+heavensfavorite@users.noreply.github.com>
2025-11-17 22:24:15 -0800

    changed wrong test, changed to orginal

--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	"encoding/gob"
 	"net/http"
+	"os"
 	"testing"
-	"text/template"
-
-	"github.com/alexedwards/scs/v2"
-	"github.com/heavensfavorite/bookings/internal/models"
 )
 
-// use the app variable declared in main.go; do not redeclare it here
-var testSession *scs.SessionManager
-var pathToTemplates = "./../../templates"
-
-var functions = template.FuncMap{
-	"humanDate":  render.humanDate,
-	"formatDate": render.formatDate,
-	"iterate":    render.iterate,
-	"add":        render.add,
-}
-
 func TestMain(m *testing.M) {
-	gob.Register(models.Reservation{})
-	gob.Register(models.User{})
-	gob.Register(models.Room{})
-	gob.Register(models.Restriction{})
-	gob.Register(map[string]int{})
+
+	os.Exit(m.Run())
 }
 
 type myHandler struct{}
